5b53fc06006982e2d900032c95371fcc406c5778
TheKingsJrester <jrester379@gmail.com>
2018-06-24 13:12:01 +0200

    Initial

---
+++ src/crystal-math.cr
@@ -0,0 +1,6 @@
+require "./crystal-math/*"
+
+# TODO: Write documentation for `Crystal::Math`
+module Crystal::Math
+  # TODO: Put your code here
+end
---
+++ src/crystal-math/version.cr
@@ -0,0 +1,3 @@
+module Crystal::Math
+  VERSION = "0.1.0"
+end
